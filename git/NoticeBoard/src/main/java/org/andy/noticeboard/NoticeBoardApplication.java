490e32ba5cce1e4669b0f5abb2f562c9f9579f43
andy3400 <grayhat3400@gmail.com>
2024-10-21 23:56:16 +0900

    Init : first commit

--- git/NoticeBoard/src/main/java/org/andy/noticeboard/NoticeBoardApplication.java
+++ git/NoticeBoard/src/main/java/org/andy/noticeboard/NoticeBoardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NoticeBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoticeBoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeBoardApplication.class, args);
+    }
 
 }
